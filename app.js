a769018557930449bd2b5d1de4acf6e3886e93e3
Anastasia Talalaeva <LightTross@yandex.ru>
2023-06-20 14:49:19 +0300

    fix cors

--- app.js
+++ app.js
@@ -35,21 +35,6 @@ app.use(cors({
   ],
 }));
 
-app.use(cors());
-app.use((req, res, next) => {
-  res.header({
-    'Access-Control-Allow-Origin': [
-      'http://api.talalayeva.promovies.nomoredomains.rocks/',
-      'https://api.talalayeva.promovies.nomoredomains.rocks/',
-      'http://talalayeva.promovies.nomoredomains.rocks/',
-      'https://talalayeva.promovies.nomoredomains.rocks/',
-      'localhost:3000',
-      'http://localhost:3000/',
-      'https://localhost:3000/',
-    ],
-  }); next();
-});
-
 app.use(cookieParser());
 
 app.use(bodyParser.json());
